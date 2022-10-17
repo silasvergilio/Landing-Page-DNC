b32628516950a8824e05768613e1ad28ca48e1d7
Silas Vergilio <silas.vergilio@gmail.com>
2022-10-17 00:02:17 -0300

    feat/keyboard event listener

--- main.js
+++ main.js
@@ -5,8 +5,64 @@ function addRedBorder(id) {
 }
 
 function highlightCard(cardId) {
-    console.log("mouseover");
     var element = document.getElementById(cardId);
     element.classList.toggle("card-highlight");
 
 }
+
+function checkKeyboardCode()
+{
+    document.addEventListener('keydown', (event) => {
+        var name = event.key;
+        var code = event.code;
+        // Alert the key name and key code on keydown
+        alert(`Key pressed ${name} \r\n Key code value: ${code}`);
+      }, false);
+      
+}
+
+function addKeyboardEventListeners()
+{
+    document.addEventListener('keydown', (event) => {
+
+        var ingresso1 = document.getElementById("quinta");
+        var ingresso2 = document.getElementById("sexta");
+        var ingresso3 = document.getElementById("sabado");
+        var ingresso4 = document.getElementById("domingo");
+
+
+        var code = event.code;
+        if(code == 'Digit1') {
+            ingresso1.classList.toggle("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.remove("card-highlight"); 
+        }
+
+        if(code == 'Digit2') {
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.toggle("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.remove("card-highlight"); 
+        }
+
+        if(code == 'Digit3') {
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.toggle("card-highlight");
+            ingresso4.classList.remove("card-highlight"); 
+        }
+
+        
+        if(code == 'Digit4') {
+            ingresso1.classList.remove("card-highlight");
+            ingresso2.classList.remove("card-highlight");
+            ingresso3.classList.remove("card-highlight");
+            ingresso4.classList.toggle("card-highlight"); 
+        }
+
+      }, false);
+
+}
+
+addKeyboardEventListeners();
